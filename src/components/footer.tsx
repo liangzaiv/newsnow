5a2e35c6dbb7058992dd5040ecbed20204bde80e
liangzaiv <liangzaivvv@gmail.com>
2025-04-02 22:08:06 +0800

    Update footer.tsx

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,10 @@
 export function Footer() {
   return (
     <>
-      <a href=https://coe.minus1.top/ target="_blank">coe.minus1.top</a>
+      <a href="https://coe.minus1.top/" target="_blank">coe.minus1.top</a>
       <span>
         <span>NewsNow © 2024 By </span>
-        <a href=https://coe.minus1.top/ target="_blank">
+        <a href="https://coe.minus1.top/" target="_blank">
           coe.minus1.top
         </a>
       </span>
